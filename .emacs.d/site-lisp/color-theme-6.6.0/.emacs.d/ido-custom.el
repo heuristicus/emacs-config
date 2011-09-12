a6414ddce8349a06f24762f59aa0fe1b05a40b01
Michal Staniaszek <m.staniaszek@gmail.com>
2011-09-12 16:34:27 +0900

    removed wrongly copied files

--- .emacs.d/site-lisp/color-theme-6.6.0/.emacs.d/ido-custom.el
+++
@@ -1,8 +0,0 @@
-(require 'ido)
-
-(setq ido-flex-matching t)
-(setq ido-everywhere t)
-(setq ido-file-extensions-order '(".py" ".tex" ".org" ".txt"))
-(ido-mode 1)
-
-(provide 'ido-custom)
--- .emacs.d/site-lisp/color-theme-6.6.0/.emacs.d/latex-custom.el
+++
@@ -1,5 +0,0 @@
-(load "auctex.el" nil t t)
-(load "preview-latex.el" nil t t)
-(setq TeX-PDF-mode t)
-
-(provide 'latex-custom)
