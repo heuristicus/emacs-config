b1f4b7c34b8586b150402440ed6aea4771395a54
Michal Staniaszek <m.staniaszek@gmail.com>
2011-11-04 22:17:19 +0900

    messed around a bit

--- .emacs.d/rainbow-delimiters.el
+++ .emacs.d/rainbow-delimiters.el
@@ -138,6 +138,28 @@
 
 (eval-when-compile (require 'cl))
 
+(defmacro with-silent-modifications (&rest body)
+  "Execute BODY, pretending it does not modify the buffer.
+If BODY performs real modifications to the buffer's text, other
+than cosmetic ones, undo data may become corrupted.
+Typically used around modifications of text-properties which do not really
+affect the buffer's content."
+  (declare (debug t) (indent 0))
+  (let ((modified (make-symbol "modified")))
+    `(let* ((,modified (buffer-modified-p))
+            (buffer-undo-list t)
+            (inhibit-read-only t)
+            (inhibit-modification-hooks t)
+            deactivate-mark
+            ;; Avoid setting and removing file locks and checking
+            ;; buffer's uptodate-ness w.r.t the underlying file.
+            buffer-file-name
+            buffer-file-truename)
+       (unwind-protect
+           (progn
+             ,@body)
+         (unless ,modified
+           (restore-buffer-modified-p nil))))))
 
 ;; Note: some of the functions in this file have been inlined for speed.
 ;; Inlining functions can cause problems with debugging. To debug these
