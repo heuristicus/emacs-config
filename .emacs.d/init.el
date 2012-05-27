3dbda1bcaf9e0a78681f9ca211125aa8eda0bba7
Michal Staniaszek <m.staniaszek@gmail.com>
2012-05-27 21:39:38 +0900

    transparency not working.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -75,10 +75,20 @@
 (blink-cursor-mode nil)
 
 ;; AUCTeX evince viewer
-
 (setq TeX-output-view-style
       (quote
        (("^pdf$" "." "evince %o"))))
 
 ;; Start emacs server
 (server-start)
+
+(defun set-frame-alpha (arg &optional active)
+  (interactive "nEnter alpha value (1-100): \np")
+  (let* ((elt (assoc 'alpha default-frame-alist))
+         (old (frame-parameter nil 'alpha))
+         (new (cond ((atom old)     `(,arg ,arg))
+                    ((eql 1 active) `(,arg ,(cadr old)))
+                    (t              `(,(car old) ,arg)))))
+    (if elt (setcdr elt new) (push `(alpha ,@new) default-frame-alist))
+    (set-frame-parameter nil 'alpha new)))
+(global-set-key (kbd "C-c t") 'set-frame-alpha)
