e07ee420fb80e5058cf98ef1318c6bc3c2bbce8e
Michal Staniaszek <m.staniaszek@gmail.com>
2012-05-30 12:57:17 +0900

    added keybinds, yas stuff in init

--- .emacs.d/keybind-custom.el
+++ .emacs.d/keybind-custom.el
@@ -1,4 +1,4 @@
 (global-set-key (kbd "<f2> k") 'japanese-katakana-region)
 (global-set-key (kbd "<f2> r") 'replace-string)
-
+(global-set-key (kbd "<f2> R") 'query-replace)
 (provide 'keybind-custom)
